c0678e759a4ef770d06a70da3423416b1bb4eb81
Nobody <nobodyawesomer@proton.me>
2022-07-04 16:57:51 -0700

    Refactored Morse encoding/decoding.
    I had fallen prey to premature optimization.

--- main/morse.c
+++ main/morse.c
@@ -8,87 +8,78 @@
  * @copyright Copyright (c) 2022
  *
  */
+#include <stddef.h>
 #include "morse.h"
+
+const morse_char_t morses[] = {
+		{'A', {DIT, DAH, NIL}},
+		{'B', {DAH, DIT, DIT, DIT, NIL}},
+		{'C', {DAH, DIT, DAH, DIT, NIL}},
+		{'D', {DAH, DIT, DIT, NIL}},
+		{'E', {DIT, NIL}},
+		{'F', {DIT, DIT, DAH, DIT, NIL}},
+		{'G', {DAH, DAH, DIT, NIL}},
+		{'H', {DIT, DIT, DIT, DIT, NIL}},
+		{'I', {DIT, DIT, NIL}},
+		{'J', {DIT, DAH, DAH, DAH, NIL}},
+		{'K', {DAH, DIT, DAH, NIL}},
+		{'L', {DIT, DAH, DIT, DIT, NIL}},
+		{'M', {DAH, DAH, NIL}},
+		{'N', {DAH, DIT, NIL}},
+		{'O', {DAH, DAH, DAH, NIL}},
+		{'P', {DIT, DAH, DAH, DIT, NIL}},
+		{'Q', {DAH, DAH, DIT, DAH, NIL}},
+		{'R', {DIT, DAH, DIT, NIL}},
+		{'S', {DIT, DIT, DIT, NIL}},
+		{'T', {DAH, NIL}},
+		{'U', {DIT, DIT, DAH, NIL}},
+		{'V', {DIT, DIT, DIT, DAH, NIL}},
+		{'W', {DIT, DAH, DAH, NIL}},
+		{'X', {DAH, DIT, DIT, DAH, NIL}},
+		{'Y', {DAH, DIT, DAH, DAH, NIL}},
+		{'Z', {DAH, DAH, DIT, DIT, NIL}},
+
+		{'0', {DAH, DAH, DAH, DAH, DAH, NIL}},
+		{'1', {DIT, DAH, DAH, DAH, DAH, NIL}},
+		{'2', {DIT, DIT, DAH, DAH, DAH, NIL}},
+		{'3', {DIT, DIT, DIT, DAH, DAH, NIL}},
+		{'4', {DIT, DIT, DIT, DIT, DAH, NIL}},
+		{'5', {DIT, DIT, DIT, DIT, DIT, NIL}},
+		{'6', {DAH, DIT, DIT, DIT, DIT, NIL}},
+		{'7', {DAH, DAH, DIT, DIT, DIT, NIL}},
+		{'8', {DAH, DAH, DAH, DIT, DIT, NIL}},
+		{'9', {DAH, DAH, DAH, DAH, DIT, NIL}},
+
+		// {'\0', {NIL}} // nul terminated lol
+};
 
 morse_char_t encode_morse(char ch)
 {
-	switch (ch)
+	const size_t len = sizeof(morses) / sizeof(morses[0]);
+	for (size_t i = 0; i < len; i++)
 	{
-	case 'A':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DAH, NIL}};
-	case 'B':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DIT, DIT, DIT, NIL}};
-	case 'C':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DIT, DAH, DIT, NIL}};
-	case 'D':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DIT, DIT, NIL}};
-	case 'E':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, NIL}};
-	case 'F':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DIT, DAH, DIT, NIL}};
-	case 'G':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DAH, DIT, NIL}};
-	case 'H':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DIT, DIT, DIT, NIL}};
-	case 'I':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DIT, NIL}};
-	case 'J':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DAH, DAH, DAH, NIL}};
-	case 'K':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DAH, DIT, NIL}};
-	case 'L':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DAH, DIT, DIT, NIL}};
-	case 'M':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DAH, NIL}};
-	case 'N':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DIT, NIL}};
-	case 'O':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DAH, DAH, NIL}};
-	case 'P':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DAH, DAH, DIT, NIL}};
-	case 'Q':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DAH, DIT, DAH, NIL}};
-	case 'R':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DAH, DIT, NIL}};
-	case 'S':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DIT, DIT, NIL}};
-	case 'T':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, NIL}};
-	case 'U':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DIT, DAH, NIL}};
-	case 'V':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DIT, DIT, DAH, NIL}};
-	case 'W':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DAH, DAH, NIL}};
-	case 'X':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DIT, DIT, DAH, NIL}};
-	case 'Y':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DIT, DAH, DAH, NIL}};
-	case 'Z':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DAH, DIT, DIT, NIL}};
-
-	case '0':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DAH, DAH, DAH, DAH, NIL}};
-	case '1':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DAH, DAH, DAH, DAH, NIL}};
-	case '2':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DIT, DAH, DAH, DAH, NIL}};
-	case '3':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DIT, DIT, DAH, DAH, NIL}};
-	case '4':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DIT, DIT, DIT, DAH, NIL}};
-	case '5':
-		return (morse_char_t){.orig = ch, .symbols = {DIT, DIT, DIT, DIT, DIT, NIL}};
-	case '6':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DIT, DIT, DIT, DIT, NIL}};
-	case '7':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DAH, DIT, DIT, DIT, NIL}};
-	case '8':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DAH, DAH, DIT, DIT, NIL}};
-	case '9':
-		return (morse_char_t){.orig = ch, .symbols = {DAH, DAH, DAH, DAH, DIT, NIL}};
-
-	default:
-		return (morse_char_t){.orig = '\0', .symbols = {NIL}};
+		if (morses[i].orig == ch)
+			return morses[i];
 	}
+	return (morse_char_t){'\0', {NIL}};
+}
+
+char decode_morse(uint8_t symbols[])
+{
+	// size_t possibles[]; // array of possible indices
+	const size_t len = sizeof(morses) / sizeof(morses[0]);
+	for (size_t i = 0; i < len; i++)
+	{
+		// Compare loop
+		for (size_t j = 0; j < MAX_SYMBOLS_CHAR; j++)
+		{
+			if (symbols[j] != morses[i].symbols[j])
+				goto OUTER;					 // 'continue outer'
+			if (symbols[j] == NIL) // if both are NIL, then return match
+				break;
+		}
+		return morses[i].orig;
+	OUTER:;
+	}
+	return '\0';
 }
--- main/morse.h
+++ main/morse.h
@@ -15,10 +15,10 @@
 
 #define MAX_SYMBOLS_CHAR ((size_t)8) ///< Maximum symbols that can represent a character in Morse
 
-// TODO: configify this
 #define SYMBOL_SPACE 1 ///< The gap between two symbols (dit/dah)
-#define CHAR_SPACE 3	 ///< The gap between two characters within a word
-#define WORD_SPACE 7	 ///< The gap between two words
+//#define CHAR_SPACE 3	 ///< The gap between two characters within a word
+//#define WORD_SPACE 7	 ///< The gap between two words
+// ^ configified
 
 #define DIT ((uint8_t)1) ///< Morse "dit"/dot
 #define DAH ((uint8_t)3) ///< Morse "dah"/dash
@@ -42,5 +42,14 @@ typedef struct morse_char_s
  * @return morse_char_t
  */
 morse_char_t encode_morse(char ch);
+
+/**
+ * @brief Get the character representation of an array of Morse symbols.
+ * Should be terminated with NIL.
+ *
+ * @param symbols the array of DITs and DAHs, NIL terminated
+ * @return char
+ */
+char decode_morse(uint8_t symbols[]);
 
 #endif /* __MORSE_H */
